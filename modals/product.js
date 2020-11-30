400c3d453e79e8cb7ba29e8c65d16d833efc12a8
saqibarshad1k <saqibarshad1k@gmail.com>
2020-11-30 17:09:25 +0500

    Structural changes

--- modals/product.js
+++ modals/product.js
@@ -27,9 +27,16 @@ const subcategorySchema = new mongoose.Schema({
     image: {
         type:String
     },
-    maincategoryname: {
-        type:  mongoose.Schema.Types.ObjectId,
-        required: true
+    mainCategory: {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        maincategoryname:{
+            type: String,
+            required: true
+
+        }
     }
 });
 
@@ -45,40 +52,41 @@ const subsubcategorySchema = new mongoose.Schema({
     image: {
         type:String
     },
-    maincategoryname: {
-        type:  mongoose.Schema.Types.ObjectId,
-        required: true
+    mainCategory: {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        maincategoryname:{
+            type: String,
+            required: true
+
+        }
     },
-    subcategoryname: {
-        type:  mongoose.Schema.Types.ObjectId,
-        required: true
+    subCategory: {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        subcategoryname:{
+            type: String,
+            required: true
+
+        }
+    },
+    subsubCategory: {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true
+        },
+        subsubcategoryname:{
+            type: String,
+            required: true
+
+        }
     }
 });
 
-
-
-// For Talha
-// const subcategorySchema = new mongoose.Schema({
-//     subcategoryname:{
-//         type: String,
-//         minlength:1,
-//         maxlength:50,
-//         required: true
-//     },
-//     image: {
-//         type:String
-//     },
-//     maincategoryname:{
-//         type: String,
-//         minlength:1,
-//         maxlength:50,
-//         required: true
-//     }
-// });
-
-
-
-// extended
 const productSchema = new mongoose.Schema({
 
     
@@ -125,57 +133,6 @@ const productSchema = new mongoose.Schema({
 });
 
 
-
-// For Talha
-// const productSchema = new mongoose.Schema({
-
-    
-//     productname:{
-//         type: String,
-//         maxlength: 50, 
-//         minlength: 5,
-//         required: true
-//     },
-//     productprice:{
-//         type: String,
-//         maxlength: 6,
-//         minlength: 1,
-//         required: true
-//     },
-//     companyname:{
-//         type: String,
-//         maxlength: 50,
-//         minlength: 5,
-//         required: true
-//     },
-//     maincategory: {
-//         type: String,
-//         minlength: 1,
-//         maxlength: 50,
-//         required: true
-//     },
-//     subcategory: {
-//         type: String,
-//         minlength: 1,
-//         maxlength: 50,
-//         required: true
-//     },
-//     image:{
-//         type: String,
-//         default: null
-//     },
-//     Type:{
-//         type: String,
-//         minlength: 1,
-//         maxlength: 50,
-//         required: true
-//     }
-// });
-
-
-
-
-
 const Product = mongoose.model("product", productSchema);
 const Maincategory = mongoose.model("maincategory", maincategorySchema);
 const Subcategory = mongoose.model("subcategory", subcategorySchema);
@@ -188,33 +145,15 @@ function productValidation(credentials){
         productname: Joi.string().min(1).max(50).required(),
         productprice: Joi.string().min(1).max(6).required(),
         companyname: Joi.string().min(1).max(50).required(),
-        maincategoryname: Joi.objectId().required(),
-        subcategoryname: Joi.objectId().required(),
-        subsubcategoryname: Joi.objectId().required(),
+        maincategoryname: Joi.required(),
+        subcategoryname: Joi.required(),
+        subsubcategoryname: Joi.required(),
         type: Joi.string().min(1).max(50).required(),
         image: Joi.string(),
         
     }
     return Joi.validate(credentials, schema);
 };
-
-
-// for talha
-// function productValidation(credentials){
-//     const schema = {
-//         // cart: Joi.object(),
-//         productname: Joi.string().min(5).max(50).required(),
-//         productprice: Joi.string().min(1).max(6).required(),
-//         companyname: Joi.string().min(5).max(50).required(),
-//         maincategory: Joi.string().min(1).max(50).required(),
-//         subcategory: Joi.string().min(1).max(50).required(),
-//         Type: Joi.string().min(1).max(50).required(),
-//         image: Joi.string(),
-        
-//     }
-
-//     return Joi.validate(credentials, schema);
-// };
 
 function maincategoryValidation(credentials){
     const schema = {
@@ -232,8 +171,8 @@ function subsubcategoryValidation(credentials){
     const schema = {
        
         subsubcategoryname: Joi.string().min(1).max(50).required(),
-        maincategoryname: Joi.objectId().required(),
-        subcategoryname: Joi.objectId().required(),
+        mainCategory: Joi.required(),
+        subCategory: Joi.required(),
         image: Joi.string()
     }
     return Joi.validate(credentials, schema);
@@ -245,23 +184,11 @@ function subcategoryValidation(credentials){
     const schema = {
        
         subcategoryname: Joi.string().min(1).max(50).required(),
-        maincategoryname: Joi.objectId().required(),
+        mainCategory: Joi.required(),
         image: Joi.string()
     }
     return Joi.validate(credentials, schema);
 };
-
- // for Talha
-// function subcategoryValidation(credentials){
-//     const schema = {
-       
-//         subcategoryname: Joi.string().min(1).max(50).required(),
-//         maincategoryname: Joi.string().min(1).max(50).required(),
-//         image: Joi.string()
-//     }
-
-// return Joi.validate(credentials, schema);
-// };
 
 
 module.exports.Product = Product;
