fd89d3d93907e7cb9f456e1ce60c2801ed14aee3
saqibarshad1k <saqibarshad1k@gmail.com>
2020-12-05 04:02:22 +0500

    Web-socket

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://supermartwebapp.herokuapp.com/customers",
+  //  origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
