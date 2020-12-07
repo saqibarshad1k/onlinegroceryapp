2ad4d7c10f85e81460fde43736128d18f9524db1
saqibarshad1k <saqibarshad1k@gmail.com>
2020-12-07 23:27:12 +0500

    socket test

--- sockets/test.js
+++ sockets/test.js
@@ -1,6 +1,6 @@
 
 exports = module.exports = function(io){
-    io.sockets.on('connection', function (socket) {
+    io.on('connection', function (socket) {
       socket.on('test', function () {
         socket.emit('ditConsumer', "hahahah");
         console.log('testEvent triggered--------------------------------------------------------------------------------');
