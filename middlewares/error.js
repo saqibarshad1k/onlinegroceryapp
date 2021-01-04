f775db303f5eda1f034404c1673aab95320c2d6c
saqibarshad1k <saqibarshad1k@gmail.com>
2021-01-04 20:07:06 +0500

    haha

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,4 +1,4 @@
 module.exports = function(err, req, res, next){
  
-   return res.status(500).send("Some error occured inside express. The error is below:");
+   return res.status(500).send(err);
 }
